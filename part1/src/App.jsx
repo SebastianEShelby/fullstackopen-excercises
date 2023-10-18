3fb77557164bc9caeb69da61ab0253c74938adf1
Sebastian Shelby <sebastianeshelby@gmail.com>
2023-10-18 11:20:53 -0400

    do not render objects

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -1,27 +1,16 @@
-// Component names must be capitalized
-const Hello = (props) => {
-  console.log(props)
+const App = () => {
+  const friends = [
+    { name: 'Peter', age: 4 },
+    { name: 'Maya', age: 10 },
+  ]
+
   return (
     <div>
-      <p>Hello {props.name}, you are {props.age} years old</p>
+      {/* {friends[0]} will NOT work because Objects are NOT valid as a React child */}
+      <p>{friends[0].name} {friends[0].age}</p>
+      <p>{friends[1].name} {friends[1].age}</p>
     </div>
   )
 }
 
-const App = () => {
-  const name = 'Peter'
-  const age = 10
-
-  return (
-    // Adjacent JSX elements must be wrapped in an enclosing tag. Here we use a "fragment" to wrap the adjacent elements. We could also return an array of html elements instead
-    <>
-      <h1>Greetings</h1>
-      {/* ALL html tags in JSX must close. */}
-      <br />
-      <Hello name='Maya' age={26+10}/>
-      <Hello name={name} age={age}/>
-    </>
-  )
-}
-
-export default App;
+export default App
