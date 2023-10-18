15181b4b1d3c5f3525905349906fc2ba73f8a287
Sebastian Shelby <sebastianeshelby@gmail.com>
2023-10-18 11:21:54 -0400

    rendering arrays

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -1,14 +1,11 @@
 const App = () => {
-  const friends = [
-    { name: 'Peter', age: 4 },
-    { name: 'Maya', age: 10 },
-  ]
+  const friends = [ 'Peter', 'Maya']
 
   return (
     <div>
-      {/* {friends[0]} will NOT work because Objects are NOT valid as a React child */}
-      <p>{friends[0].name} {friends[0].age}</p>
-      <p>{friends[1].name} {friends[1].age}</p>
+      {/* React also allows arrays to be rendered if the array contains values ​​that are eligible for rendering */}
+      {/* results in: PeterMaya */}
+      <p>{friends}</p>
     </div>
   )
 }
