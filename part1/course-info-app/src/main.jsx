6235cc3b0a07a58908172afdaba6a4619a23025b
Sebastian Shelby <sebastianeshelby@gmail.com>
2023-10-18 20:01:55 -0400

    rename folder

--- part1/course-info-app/src/main.jsx
+++ part1/course-info-app/src/main.jsx
@@ -1,4 +1,4 @@
-import ReactDOM from 'react-dom/client';
+import ReactDOM from '../../part1-playground/node_modules/@types/react-dom/client';
 import App from './App'
 
-ReactDOM.createRoot(document.getElementById('root')).render(<App/>)
+ReactDOM.createRoot(document.getElementById('root')).render(<App />)
