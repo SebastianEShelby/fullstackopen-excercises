0062e075b9db022f5628c38851bb7752891cbb88
Sebastian Shelby <sebastianeshelby@gmail.com>
2023-10-20 12:45:19 -0400

    part 1.c component helper functions

--- part1/part1-playground/src/App.jsx
+++ part1/part1-playground/src/App.jsx
@@ -1,11 +1,30 @@
-const App = () => {
-  const friends = [ 'Peter', 'Maya']
+const Hello = (props) => {
+
+  const bornYear = () => {
+    const yearNow = new Date().getFullYear()
+    return yearNow - props.age
+  }
 
   return (
     <div>
-      {/* React also allows arrays to be rendered if the array contains values ​​that are eligible for rendering */}
-      {/* results in: PeterMaya */}
-      <p>{friends}</p>
+      <p>
+        Hello {props.name}, you are {props.age} years old
+      </p>
+
+      <p>So you were probably born in {bornYear()}</p>
+    </div>
+  )
+}
+
+const App = () => {
+  const name = 'Peter'
+  const age = 10
+
+  return (
+    <div>
+      <h1>Greetings</h1>
+      <Hello name="Maya" age={26 + 10} />
+      <Hello name={name} age={age} />
     </div>
   )
 }
