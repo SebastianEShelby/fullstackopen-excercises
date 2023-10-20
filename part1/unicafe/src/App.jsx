df4fcc931cd1042dee41014ea90a7a3638d38290
Sebastian Shelby <sebastianeshelby@gmail.com>
2023-10-20 18:07:35 -0400

    part 1.7: unicafe step2

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -20,12 +20,20 @@ const Feedback = ({ feedbacks }) => {
 }
 
 const Statistics = ({ good, neutral, bad }) => {
+
+  const all = good + neutral + bad;
+  const average = (good - bad) / all;
+  const positive = good * 100 / all;
+
   return (
     <>
       <h1>statistics</h1>
       <p>good {good}</p>
       <p>neutral {neutral}</p>
       <p>bad {bad}</p>
+      <p>all {all}</p>
+      <p>average {average}</p>
+      <p>positive {positive} %</p>
     </>
   )
 }
