2aa70527aecd4d89e6294747081fbd41cb0ac700
Sebastian Shelby <sebastianeshelby@gmail.com>
2023-10-20 13:08:31 -0400

    part 1.c page re-rendering

--- part1/part1-playground/src/main.jsx
+++ part1/part1-playground/src/main.jsx
@@ -1,4 +1,24 @@
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
 
-ReactDOM.createRoot(document.getElementById('root')).render(<App />)
+import App from './App'
+
+let counter = 1
+
+const root = ReactDOM.createRoot(document.getElementById('root'))
+
+const refresh = () => {
+	root.render(
+		<App counter={counter} />
+	)
+	console.log('counter', counter)
+
+	if (counter == 5) {
+		console.log("counter is 5");
+		clearInterval(intervalId);
+	}
+}
+
+const intervalId = setInterval(() => {
+	refresh()
+	counter += 1
+}, 1000)
