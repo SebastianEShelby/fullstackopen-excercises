138d53e55e6903de43946d3ddb21d82be51ef4b4
Sebastian Shelby <sebastianeshelby@gmail.com>
2023-10-20 18:22:08 -0400

    part 1.11*: unicafe step6

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -22,7 +22,11 @@ const Feedback = ({ feedbacks }) => {
 const StatisticLine = ({ text, value }) => {
 
   return (
-    <p>{text} {value} {text == 'positive' && '%'}</p>
+    <tr>
+      <td>{text}</td>
+      <td>{value} {text == 'positive' && '%'}</td>
+    </tr>
+
   )
 }
 
@@ -42,12 +46,16 @@ const Statistics = ({ good, neutral, bad }) => {
   return (
     <>
       <h1>statistics</h1>
-      <StatisticLine text='good' value={good} />
-      <StatisticLine text='neutral' value={neutral} />
-      <StatisticLine text='bad' value={bad} />
-      <StatisticLine text='all' value={all} />
-      <StatisticLine text='average' value={average} />
-      <StatisticLine text='positive' value={positive} />
+      <table>
+        <tbody>
+          <StatisticLine text='good' value={good} />
+          <StatisticLine text='neutral' value={neutral} />
+          <StatisticLine text='bad' value={bad} />
+          <StatisticLine text='all' value={all} />
+          <StatisticLine text='average' value={average} />
+          <StatisticLine text='positive' value={positive} />
+        </tbody>
+      </table>
     </>
   )
 }
