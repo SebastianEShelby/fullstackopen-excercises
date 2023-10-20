5c12bc72726fe4bc8a87cda6d2dac4e58e8ff55e
Sebastian Shelby <sebastianeshelby@gmail.com>
2023-10-20 17:23:27 -0400

    part 1.d A function that returns a function v2

--- part1/part1-playground/src/App.jsx
+++ part1/part1-playground/src/App.jsx
@@ -3,22 +3,23 @@ import { useState } from "react"
 const App = () => {
   const [value, setValue] = useState(10)
 
-  // Functions returning functions can be utilized in defining generic functionality that can be customized with parameters. The hello function that creates the event handlers can be thought of as a factory that produces customized event handlers meant for greeting users.
-  const hello = (who) => {
-    const handler = () => {
-      console.log('hello', who)
-    }
-
-    return handler
+  const setToValue = (newValue) => {
+    console.log('value now', newValue)
+    setValue(newValue)
   }
 
   return (
     <div>
       {value}
-
-      <button onClick={hello('world')}>button</button>
-      <button onClick={hello('react')}>button</button>
-      <button onClick={hello('function')}>button</button>
+      <button onClick={() => setToValue(1000)}>
+        thousand
+      </button>
+      <button onClick={() => setToValue(0)}>
+        reset
+      </button>
+      <button onClick={() => setToValue(value + 1)}>
+        increment
+      </button>
     </div>
   )
 }
