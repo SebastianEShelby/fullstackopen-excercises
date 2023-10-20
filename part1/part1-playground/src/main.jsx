3277b525ea0c4ba468025c9e03e1f1fcc6079f29
Sebastian Shelby <sebastianeshelby@gmail.com>
2023-10-20 13:20:05 -0400

    part 1.c stateful component

--- part1/part1-playground/src/main.jsx
+++ part1/part1-playground/src/main.jsx
@@ -2,23 +2,4 @@ import ReactDOM from 'react-dom/client'
 
 import App from './App'
 
-let counter = 1
-
-const root = ReactDOM.createRoot(document.getElementById('root'))
-
-const refresh = () => {
-	root.render(
-		<App counter={counter} />
-	)
-	console.log('counter', counter)
-
-	if (counter == 5) {
-		console.log("counter is 5");
-		clearInterval(intervalId);
-	}
-}
-
-const intervalId = setInterval(() => {
-	refresh()
-	counter += 1
-}, 1000)
+ReactDOM.createRoot(document.getElementById('root')).render(<App />)
