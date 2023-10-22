4d17a5fbaf7c4efd3e5f235b2b367ad99e316369
Sebastian Shelby <sebastianeshelby@gmail.com>
2023-10-22 14:36:52 -0400

    part 2.1: Course information step6

---
+++ part2/course-info/src/components/Course.jsx
@@ -0,0 +1,30 @@
+const Header = ({ course }) => <h1>{course}</h1>
+
+const Part = ({ part }) => {
+	return (
+		<p>
+			{part.name} {part.exercises}
+		</p>
+	)
+}
+
+const Content = ({ parts }) => {
+	return (
+		<>
+			{parts.map(part =>
+				<Part key={part.id} part={part} />
+			)}
+		</>
+	)
+}
+
+const Course = ({ course }) => {
+	return (
+		<div>
+			<Header course={course.name} />
+			<Content parts={course.parts} />
+		</div>
+	)
+}
+
+export default Course
