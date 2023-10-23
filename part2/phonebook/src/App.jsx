9755d181d21ca0ffded70fec4e518d529ca06d1d
Sebastian Shelby <sebastianeshelby@gmail.com>
2023-10-23 13:13:42 -0400

    part 2.9*: The Phonebook Step4

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -2,15 +2,21 @@ import { useState } from 'react'
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '416-111-2222' }
+    { name: 'Arto Hellas', number: '040-123456', id: 1 },
+    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
   ])
+
   const [newName, setNewName] = useState('');
-  const [newNumber, setNewNumber] = useState('')
+  const [newNumber, setNewNumber] = useState('');
+  const [newFilter, setNewFilter] = useState('');
+  const [filteredPersons, setFilteredPersons] = useState(persons);
 
   const addName = (event) => {
     event.preventDefault()
     if (isDuplicateName()) return alert(`${newName} is already added to phonebook`)
-    const newPerson = { name: newName, number: newNumber }
+    const newPerson = { name: newName, number: newNumber, id: persons.length + 1 }
     setPersons(persons.concat(newPerson));
     setNewName('');
   }
@@ -22,12 +28,19 @@ const App = () => {
   }
 
   const handleNameChange = (event) => setNewName(event.target.value)
-
   const handleNewNumber = (event) => setNewNumber(event.target.value)
+  const handleNewFilter = (event) => {
+    const newFilter = event.target.value;
+    const filteredPersons = persons.filter(person => person.name.toLocaleLowerCase().match(newFilter.toLocaleLowerCase()))
+    setFilteredPersons(filteredPersons);
+    return setNewFilter(newFilter)
+  }
 
   return (
     <div>
       <h2>Phonebook</h2>
+      <p>filter shown with <input value={newFilter} onChange={handleNewFilter} /></p>
+      <h2>add a new</h2>
       <form onSubmit={addName}>
         <div>
           name: <input value={newName} onChange={handleNameChange} />
@@ -38,7 +51,7 @@ const App = () => {
         </div>
       </form>
       <h2>Numbers</h2>
-      {persons.map(person => <p key={person.name}>{person.name} {person.number}</p>)}
+      {filteredPersons.map(person => <p key={person.id}>{person.name} {person.number}</p>)}
     </div>
   )
 }
