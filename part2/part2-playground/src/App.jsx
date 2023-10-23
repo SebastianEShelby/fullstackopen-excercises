5410bbcc0370693ecbbb02888bb54b1388df1d32
Sebastian Shelby <sebastianeshelby@gmail.com>
2023-10-23 16:09:12 -0400

    part 1.c effect-hooks v2

--- part2/part2-playground/src/App.jsx
+++ part2/part2-playground/src/App.jsx
@@ -7,20 +7,17 @@ const App = () => {
   const [newNote, setNewNote] = useState('a new note...')
   const [showAll, setShowAll] = useState(true);
 
-  const hook = () => {
+  useEffect(() => {
     console.log('effect')
-    axios
-      .get('http://localhost:3001/notes')
-      .then(response => {
-        console.log('promise fulfilled')
-        setNotes(response.data)
-      })
-  }
 
-  // By default, the effect is always run after the component has been rendered. In our case, however, we only want to execute the effect along with the first render.
+    const eventHandler = response => {
+      console.log('promise fulfilled')
+      setNotes(response.data)
+    }
 
-  // The second parameter of useEffect is used to specify how often the effect is run. If the second parameter is an empty array [], then the effect is only run along with the first render of the component.
-  useEffect(hook, [])
+    const promise = axios.get('http://localhost:3001/notes')
+    promise.then(eventHandler)
+  }, [])
 
   const notesToShow = showAll ? notes : notes.filter(note => note.important)
 
