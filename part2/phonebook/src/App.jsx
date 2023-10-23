c0a2035fa084c3d652fa48d73d0a22389762c7ef
Sebastian Shelby <sebastianeshelby@gmail.com>
2023-10-23 16:23:30 -0400

    part 2.11: The Phonebook Step6

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,20 +1,25 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import Filter from './components/filter';
 import PersonForm from './components/person-form';
 import Persons from './components/persons';
+import axios from '../node_modules/axios/index';
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456', id: 1 },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-  ])
-
+  const [persons, setPersons] = useState([])
   const [newName, setNewName] = useState('');
   const [newNumber, setNewNumber] = useState('');
   const [newFilter, setNewFilter] = useState('');
   const [filteredPersons, setFilteredPersons] = useState(persons);
+
+  useEffect(() => {
+    axios.get('http://localhost:3001/persons')
+      .then(response => {
+        console.log('response.data', response.data);
+        setPersons(response.data)
+        setFilteredPersons(response.data)
+      })
+  }, [])
+  console.log('render', persons);
 
   const addName = (event) => {
     event.preventDefault()
