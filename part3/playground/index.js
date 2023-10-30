c994d5a62ee4d3c6a7b979e72d83c1cab604c3d2
Sebastian Shelby <sebastianeshelby@gmail.com>
2023-10-30 13:42:19 -0400

    part 1.a add comments

--- part3/playground/index.js
+++ part3/playground/index.js
@@ -1,10 +1,12 @@
 const http = require('http')
 
+// The code uses the createServer method of the http module to create a new web server. An event handler is registered to the server that is called every time an HTTP request is made to the server's address http://localhost:3001.
 const app = http.createServer((request, response) => {
   response.writeHead(200, { 'Content-Type': 'text/plain' })
   response.end('Hello World')
 })
 
 const PORT = 3001
+// bind the http server assigned to the app variable, to listen to HTTP requests sent to port 3001
 app.listen(PORT)
 console.log(`Server running on port ${PORT}`)
