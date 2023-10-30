2896e5bb2bb9bc360462284d4aa97cb1925c0731
Sebastian Shelby <sebastianeshelby@gmail.com>
2023-10-30 13:47:43 -0400

    part 1.a simple web server v2

--- part3/playground/index.js
+++ part3/playground/index.js
@@ -1,12 +1,27 @@
 const http = require('http')
 
-// The code uses the createServer method of the http module to create a new web server. An event handler is registered to the server that is called every time an HTTP request is made to the server's address http://localhost:3001.
+let notes = [
+  {
+    id: 1,
+    content: "HTML is easy",
+    important: true
+  },
+  {
+    id: 2,
+    content: "Browser can execute only JavaScript",
+    important: false
+  },
+  {
+    id: 3,
+    content: "GET and POST are the most important methods of HTTP protocol",
+    important: true
+  }
+]
 const app = http.createServer((request, response) => {
-  response.writeHead(200, { 'Content-Type': 'text/plain' })
-  response.end('Hello World')
+  response.writeHead(200, { 'Content-Type': 'application/json' })
+  response.end(JSON.stringify(notes))
 })
 
 const PORT = 3001
-// bind the http server assigned to the app variable, to listen to HTTP requests sent to port 3001
 app.listen(PORT)
 console.log(`Server running on port ${PORT}`)
