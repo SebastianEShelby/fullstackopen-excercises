f52d23c99ce96437f01e635c9cc56e313dc575f4
Sebastian Shelby <sebastianeshelby@gmail.com>
2023-11-05 10:37:46 -0500

    part 3b update baseUrl

--- part2/part2-playground/src/services/notes.js
+++ part2/part2-playground/src/services/notes.js
@@ -1,6 +1,6 @@
 // @ts-ignore
 import axios from 'axios'
-const baseUrl = 'http://localhost:3001/api/notes'
+const baseUrl = '/api/notes'
 
 const getAll = () => {
 	const request = axios.get(baseUrl)
