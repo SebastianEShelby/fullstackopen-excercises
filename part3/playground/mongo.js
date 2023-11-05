966cf2c75b0cd74b8f71a25259161941be221bf7
Sebastian Shelby <sebastianeshelby@gmail.com>
2023-11-05 15:36:34 -0500

    part 3.c MongoDB

---
+++ part3/playground/mongo.js
@@ -0,0 +1,30 @@
+const mongoose = require('mongoose')
+require('dotenv').config();
+
+if (!process.env.MONGO_USERNAME || !process.env.MONGO_PASSWORD) {
+  console.log('set username and password in .env file')
+  process.exit(1)
+}
+
+const url =
+  `mongodb+srv://${process.env.MONGO_USERNAME}:${process.env.MONGO_PASSWORD}@homebrew.gbcyj2u.mongodb.net/noteApp?retryWrites=true&w=majority`
+
+mongoose.set('strictQuery', false)
+mongoose.connect(url)
+
+const noteSchema = new mongoose.Schema({
+  content: String,
+  important: Boolean,
+})
+
+const Note = mongoose.model('Note', noteSchema)
+
+const note = new Note({
+  content: 'HTML is Easy',
+  important: true,
+})
+
+note.save().then(result => {
+  console.log('note saved!', result)
+  mongoose.connection.close()
+})
