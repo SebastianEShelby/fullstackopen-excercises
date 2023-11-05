4ae262e90ff03f84873cb22d86cc9e348c2e3d89
Sebastian Shelby <sebastianeshelby@gmail.com>
2023-11-05 10:02:50 -0500

    part 3b Same origin policy and CORS

--- part3/playground/index.js
+++ part3/playground/index.js
@@ -1,5 +1,7 @@
 const express = require('express')
-const app = express()
+const cors = require('cors')
+const app = express();
+app.use(cors())
 const requestLogger = (request, response, next) => {
   console.log('Method:', request.method)
   console.log('Path:  ', request.path)
