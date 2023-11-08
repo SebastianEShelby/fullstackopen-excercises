905aae81a47f6421c91fb5368c54f0c429c4f7f2
Sebastian Shelby <sebastianeshelby@gmail.com>
2023-11-07 21:13:00 -0500

    customize eslintrc

--- part3/playground/.eslintrc.js
+++ part3/playground/.eslintrc.js
@@ -23,6 +23,14 @@ module.exports = {
 	},
 	'rules': {
 		'eqeqeq': 'error',
+		'no-trailing-spaces': 'error',
+		'object-curly-spacing': [
+			'error', 'always'
+		],
+		'arrow-spacing': [
+			'error', { 'before': true, 'after': true }
+		],
+		'no-console': 0,
 		'indent': [
 			'error',
 			'tab'
