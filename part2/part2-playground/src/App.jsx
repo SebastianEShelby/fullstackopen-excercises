077b6425954dd0187f76a9f5bd9bca0247210063
Sebastian Shelby <sebastianeshelby@gmail.com>
2023-11-07 21:16:01 -0500

    fix lint errors

--- part2/part2-playground/src/App.jsx
+++ part2/part2-playground/src/App.jsx
@@ -31,7 +31,6 @@ const App = () => {
   }
 
   const toggleImportanceOf = id => {
-    const url = `http://localhost:3001/notes/${id}`
     const note = notes.find(n => n.id === id)
     const changedNote = { ...note, important: !note.important }
 
@@ -40,7 +39,7 @@ const App = () => {
       .then(returnedNote => {
         setNotes(notes.map(note => note.id !== id ? note : returnedNote))
       })
-      .catch(error => {
+      .catch(() => {
         alert(
           `the note '${note.content}' was already deleted from server`
         )
