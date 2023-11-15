3b12d19bc0a51677dce62345686a9cd8f4091c5b
Sebastian Shelby <sebastianeshelby@gmail.com>
2023-11-15 11:56:21 -0500

    part 4.5*: helper functions and unit tests, step3

--- part4/blog-list/tests/blog-likes.test.js
+++ part4/blog-list/tests/blog-likes.test.js
@@ -1,61 +1,62 @@
 const listHelper = require('../utils/list_helper')
 
+const listWithOneBlog = [
+  {
+    id: '5a422aa71b54a676234d17f8',
+    title: 'Go To Statement Considered Harmful',
+    author: 'Edsger W. Dijkstra',
+    url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+    likes: 5,
+  }
+]
+
+const biggerList = [
+  {
+    id: '5a422a851b54a676234d17f7',
+    title: 'React patterns',
+    author: 'Michael Chan',
+    url: 'https://reactpatterns.com/',
+    likes: 7,
+  },
+  {
+    id: '5a422aa71b54a676234d17f8',
+    title: 'Go To Statement Considered Harmful',
+    author: 'Edsger W. Dijkstra',
+    url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+    likes: 5,
+  },
+  {
+    id: '5a422b3a1b54a676234d17f9',
+    title: 'Canonical string reduction',
+    author: 'Edsger W. Dijkstra',
+    url: 'http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html',
+    likes: 12,
+  },
+  {
+    id: '5a422b891b54a676234d17fa',
+    title: 'First class tests',
+    author: 'Robert C. Martin',
+    url: 'http://blog.cleancoder.com/uncle-bob/2017/05/05/TestDefinitions.htmll',
+    likes: 10,
+  },
+  {
+    id: '5a422ba71b54a676234d17fb',
+    title: 'TDD harms architecture',
+    author: 'Robert C. Martin',
+    url: 'http://blog.cleancoder.com/uncle-bob/2017/03/03/TDD-Harms-Architecture.html',
+    likes: 0,
+  },
+  {
+    id: '5a422bc61b54a676234d17fc',
+    title: 'Type wars',
+    author: 'Robert C. Martin',
+    url: 'http://blog.cleancoder.com/uncle-bob/2016/05/01/TypeWars.html',
+    likes: 2,
+  }
+]
+
+
 describe('total likes', () => {
-  const listWithOneBlog = [
-    {
-      id: '5a422aa71b54a676234d17f8',
-      title: 'Go To Statement Considered Harmful',
-      author: 'Edsger W. Dijkstra',
-      url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
-      likes: 5,
-    }
-  ]
-
-  const biggerList = [
-    {
-      id: '5a422a851b54a676234d17f7',
-      title: 'React patterns',
-      author: 'Michael Chan',
-      url: 'https://reactpatterns.com/',
-      likes: 7,
-    },
-    {
-      id: '5a422aa71b54a676234d17f8',
-      title: 'Go To Statement Considered Harmful',
-      author: 'Edsger W. Dijkstra',
-      url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
-      likes: 5,
-    },
-    {
-      id: '5a422b3a1b54a676234d17f9',
-      title: 'Canonical string reduction',
-      author: 'Edsger W. Dijkstra',
-      url: 'http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html',
-      likes: 12,
-    },
-    {
-      id: '5a422b891b54a676234d17fa',
-      title: 'First class tests',
-      author: 'Robert C. Martin',
-      url: 'http://blog.cleancoder.com/uncle-bob/2017/05/05/TestDefinitions.htmll',
-      likes: 10,
-    },
-    {
-      id: '5a422ba71b54a676234d17fb',
-      title: 'TDD harms architecture',
-      author: 'Robert C. Martin',
-      url: 'http://blog.cleancoder.com/uncle-bob/2017/03/03/TDD-Harms-Architecture.html',
-      likes: 0,
-    },
-    {
-      id: '5a422bc61b54a676234d17fc',
-      title: 'Type wars',
-      author: 'Robert C. Martin',
-      url: 'http://blog.cleancoder.com/uncle-bob/2016/05/01/TypeWars.html',
-      likes: 2,
-    }
-  ]
-
   test('of empty list is zero', () => {
     const result = listHelper.totalLikes([])
     expect(result).toBe(0)
@@ -70,4 +71,53 @@ describe('total likes', () => {
     const result = listHelper.totalLikes(biggerList)
     expect(result).toBe(36)
   })
+})
+
+describe('favourite blog', () => {
+
+  test('the returned blog object format is correct for list with one item', () => {
+    const result = listHelper.favoriteBlog(listWithOneBlog)
+    expect(result).toEqual(
+      expect.objectContaining({
+        title: expect.any(String),
+        author: expect.any(String),
+        likes: expect.any(Number)
+      })
+    )
+  })
+
+  test('the returned blog object format is correct for larger list', () => {
+    const result = listHelper.favoriteBlog(biggerList)
+    expect(result).toEqual(
+      expect.objectContaining({
+        title: expect.any(String),
+        author: expect.any(String),
+        likes: expect.any(Number)
+      })
+    )
+  })
+
+  test('of empty list is null', () => {
+    const result = listHelper.favoriteBlog([])
+    expect(result).toBe(null)
+  })
+
+  test('when list has only one blog, equals that blog', () => {
+    const result = listHelper.favoriteBlog(listWithOneBlog)
+    expect(result).toEqual({
+      title: 'Go To Statement Considered Harmful',
+      author: 'Edsger W. Dijkstra',
+      likes: 5
+    })
+  })
+
+  test('of a bigger list is calculated right', () => {
+    const result = listHelper.favoriteBlog(biggerList)
+    expect(result).toEqual({
+      title: 'Canonical string reduction',
+      author: 'Edsger W. Dijkstra',
+      likes: 12
+    },)
+  })
+
 })
--- part4/blog-list/utils/list_helper.js
+++ part4/blog-list/utils/list_helper.js
@@ -1,5 +1,35 @@
+const favoriteBlog = (blogs) => {
+  if (!blogs.length) return null
+
+  if (blogs.length === 1) return {
+    title: blogs[0].title,
+    author: blogs[0].author,
+    likes: blogs[0].likes
+  }
+
+  let mostLikes = 0
+  let blogWithMostLikes = null
+
+  blogs.forEach(blog => {
+    if (blog.likes <= mostLikes && mostLikes) return
+    mostLikes = blog.likes
+    blogWithMostLikes = blog
+  })
+
+  blogWithMostLikes = {
+    title: blogWithMostLikes.title,
+    author: blogWithMostLikes.author,
+    likes: blogWithMostLikes.likes
+  }
+
+  return blogWithMostLikes
+}
+
 const totalLikes = (blogs) => {
   if (!blogs.length) return 0
+
+  if (blogs.length === 1) return blogs[0].likes
+
   return blogs.reduce((acc, curr) => (acc + curr.likes), 0)
 }
 
@@ -10,5 +40,6 @@ const dummy = (blogs) => {
 
 module.exports = {
   dummy,
-  totalLikes
+  totalLikes,
+  favoriteBlog
 }
