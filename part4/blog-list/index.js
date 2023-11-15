050b689894d11738a3924865c1bdfb15faadc486
Sebastian Shelby <sebastianeshelby@gmail.com>
2023-11-15 10:17:42 -0500

    add env variables config

--- part4/blog-list/index.js
+++ part4/blog-list/index.js
@@ -1,3 +1,4 @@
+const config = require('./utils/config')
 const express = require('express')
 const app = express()
 const cors = require('cors')
@@ -12,8 +13,7 @@ const blogSchema = new mongoose.Schema({
 
 const Blog = mongoose.model('Blog', blogSchema)
 
-const mongoUrl = 'mongodb://localhost/bloglist'
-mongoose.connect(mongoUrl)
+mongoose.connect(config.MONGODB_URI)
 
 app.use(cors())
 app.use(express.json())
@@ -36,7 +36,6 @@ app.post('/api/blogs', (request, response) => {
     })
 })
 
-const PORT = 3001
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
+app.listen(config.PORT, () => {
+  console.log(`Server running on port ${config.PORT}`)
 })
