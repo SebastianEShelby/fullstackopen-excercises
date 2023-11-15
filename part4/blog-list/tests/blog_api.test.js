88c8fb6c4abd3aed7730dd63d0968fa6c1a6e87a
Sebastian Shelby <sebastianeshelby@gmail.com>
2023-11-15 17:24:57 -0500

    part 4.10: Blog list tests, step3

--- part4/blog-list/tests/blog_api.test.js
+++ part4/blog-list/tests/blog_api.test.js
@@ -15,7 +15,10 @@ beforeEach(async () => {
 })
 
 test('returns the correct amount of blog posts in the JSON format', async () => {
-  const response = await api.get('/api/blogs').expect(200).expect('Content-Type', /application\/json/)
+  const response = await api
+    .get('/api/blogs')
+    .expect(200)
+    .expect('Content-Type', /application\/json/)
 
   expect(response.body).toHaveLength(helper.initialBlogs.length)
 })
@@ -26,6 +29,32 @@ test('the unique identifier property of the blog posts is named id', async () =>
   response.body.forEach(blog => {
     expect(blog.id).toBeDefined()
   })
+})
+
+test('a valid blog can be added', async () => {
+  const initialBlogs = await helper.blogsInDb()
+
+  const newBlog = {
+    title: 'Testing blog http post',
+    author: 'John Doe',
+    url: 'https://duckduckgo.com/',
+    likes: 0,
+  }
+
+  await api
+    .post('/api/blogs')
+    .send(newBlog)
+    .expect(201)
+    .expect('Content-Type', /application\/json/)
+
+  const finalBlogs = await helper.blogsInDb()
+
+  expect(finalBlogs).toHaveLength(initialBlogs.length + 1)
+
+  const lastItemAdded = finalBlogs.pop()
+
+  expect(lastItemAdded).toEqual({ ...newBlog, id: lastItemAdded.id })
+
 })
 
 afterAll(async () => {
