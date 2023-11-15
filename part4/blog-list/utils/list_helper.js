a40e393a29d6f356e0905a4fc80cc2fdeaa483fd
Sebastian Shelby <sebastianeshelby@gmail.com>
2023-11-15 11:09:28 -0500

    part 4.4: helper functions and unit tests, step2

--- part4/blog-list/utils/list_helper.js
+++ part4/blog-list/utils/list_helper.js
@@ -1,8 +1,14 @@
+const totalLikes = (blogs) => {
+  if (!blogs.length) return 0
+  return blogs.reduce((acc, curr) => (acc + curr.likes), 0)
+}
+
 // eslint-disable-next-line no-unused-vars
 const dummy = (blogs) => {
   return 1
 }
 
 module.exports = {
-  dummy
+  dummy,
+  totalLikes
 }
