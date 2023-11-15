5744d06a2ea1964cbd2aaf883b765703d281f69d
Sebastian Shelby <sebastianeshelby@gmail.com>
2023-11-15 17:02:54 -0500

    part 4.8: Blog list tests, step1

---
+++ part4/blog-list/tests/blog_api.test.js
@@ -0,0 +1,25 @@
+const supertest = require('supertest')
+const mongoose = require('mongoose')
+const helper = require('./test_helper')
+const app = require('../app')
+const api = supertest(app)
+const Blog = require('../models/blog')
+
+beforeEach(async () => {
+  await Blog.deleteMany({})
+
+  for (let blog of helper.initialBlogs) {
+    let blogObj = new Blog(blog)
+    await blogObj.save()
+  }
+})
+
+test('returns the correct amount of blog posts in the JSON format', async () => {
+  const response = await api.get('/api/blogs').expect(200).expect('Content-Type', /application\/json/)
+
+  expect(response.body).toHaveLength(helper.initialBlogs.length)
+})
+
+afterAll(async () => {
+  await mongoose.connection.close()
+})
