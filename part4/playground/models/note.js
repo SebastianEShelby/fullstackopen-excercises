ebede8fe142cbbba7b682ebd5bc3fde16694918a
Sebastian Shelby <sebastianeshelby@gmail.com>
2023-11-17 16:41:51 -0500

    part 4c Mongoose schema for users

--- part4/playground/models/note.js
+++ part4/playground/models/note.js
@@ -7,7 +7,11 @@ const noteSchema = new mongoose.Schema({
     required: true
   },
   important: Boolean,
-  id: String
+  id: String,
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User'
+  }
 })
 
 noteSchema.set('toJSON', {
