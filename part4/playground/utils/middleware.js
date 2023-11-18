3a3d8611912fe2b5d83cb5b9ef66f63493f24a04
Sebastian Shelby <sebastianeshelby@gmail.com>
2023-11-17 19:09:22 -0500

    part 4d Limiting creating new notes to logged-in users

--- part4/playground/utils/middleware.js
+++ part4/playground/utils/middleware.js
@@ -3,6 +3,7 @@ const logger = require('./logger')
 const requestLogger = (request, response, next) => {
   logger.info('Method:', request.method)
   logger.info('Path:  ', request.path)
+  logger.info('Authorization Header:  ', request.headers['authorization'])
   logger.info('Body:  ', request.body)
   logger.info('---')
   next()
@@ -19,6 +20,8 @@ const errorHandler = (error, request, response, next) => {
     return response.status(400).send({ error: 'malformatted id' })
   } else if (error.name === 'ValidationError') {
     return response.status(400).json({ error: error.message })
+  } else if (error.name === 'JsonWebTokenError') {
+    return response.status(401).json({ error: error.message })
   }
 
   next(error)
