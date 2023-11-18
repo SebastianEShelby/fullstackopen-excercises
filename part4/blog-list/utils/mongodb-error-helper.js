30557ca329b34367f452dd4686ff673a3fa1b1c3
Sebastian Shelby <sebastianeshelby@gmail.com>
2023-11-17 21:34:56 -0500

    improve error handling for part 4.16

--- part4/blog-list/utils/mongodb-error-helper.js
+++ part4/blog-list/utils/mongodb-error-helper.js
@@ -1,12 +1,11 @@
 const handleServerErrors = (response, error) => {
-  if (error.message.match('E11000') && error.message.match('username')) {
-    return response.status(400).json({ error: 'Username is already in use! Please try a different username' })
-  }
-
   return response.status(500).json({ error: error.message })
 }
 
 const handleValidationErrors = (response, error) => {
+  if (error?.errors['username']?.kind === 'unique') {
+    return response.status(400).json({ error: 'Username is already in use! Please try a different username' })
+  }
   return response.status(400).json({ error: error.message })
 }
 
