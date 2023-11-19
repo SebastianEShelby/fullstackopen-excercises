79b46fa156f4f16c81d2a2240c055342c289e80a
Sebastian Shelby <sebastianeshelby@gmail.com>
2023-11-19 18:54:39 -0500

    move tokenExtractor middleware to required route

--- part4/blog-list/app.js
+++ part4/blog-list/app.js
@@ -15,10 +15,9 @@ mongoose.connect(config.MONGODB_URI)
 app.use(cors())
 app.use(express.json())
 
-app.use(middleware.tokenExtractor)
 app.use('/api/login', loginRouter)
 app.use('/api/users', usersRouter)
-app.use('/api/blogs', middleware.userExtractor, blogsRouter)
+app.use('/api/blogs', middleware.tokenExtractor, middleware.userExtractor, blogsRouter)
 app.use(middleware.errorHandler)
 
 module.exports = app
