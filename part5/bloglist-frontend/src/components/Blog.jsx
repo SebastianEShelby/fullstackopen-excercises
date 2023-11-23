101df01413fa5f785739cd366a2a7e1e61b66663
Sebastian Shelby <sebastianeshelby@gmail.com>
2023-11-22 22:45:28 -0500

    part 5.7 Blog list frontend, step7

--- part5/bloglist-frontend/src/components/Blog.jsx
+++ part5/bloglist-frontend/src/components/Blog.jsx
@@ -1,7 +1,28 @@
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import { useState } from "react"
+
+const Blog = ({ blog }) => {
+  const [isDetailedView, setIsDetailedView] = useState(false)
+
+  const toggleIsDetailedView = () => {
+    setIsDetailedView(!isDetailedView)
+  }
+
+  const blogStyle = {
+    padding: "10px 10px",
+    border: '1px solid black',
+    marginBottom: 5
+  }
+
+  return (
+    <div style={blogStyle}>
+      <>{blog.title} {blog.author} <button onClick={toggleIsDetailedView}>{isDetailedView ? "hide" : "view"}</button></>
+      <div style={{ display: isDetailedView ? "" : "none" }}>
+        <p>Url: {blog.url}</p>
+        <p>Likes: {blog.likes} <button>like</button></p>
+        <p>User: {blog.user.name}</p>
+      </div >
+    </div>
+  )
+}
 
 export default Blog
