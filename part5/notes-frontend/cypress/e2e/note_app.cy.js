e0f7e569e84d48da2dd129b9240946357dee3073
Sebastian Shelby <sebastianeshelby@gmail.com>
2023-11-24 19:45:58 -0500

    part 5d Controlling the state of the database

--- part5/notes-frontend/cypress/e2e/note_app.cy.js
+++ part5/notes-frontend/cypress/e2e/note_app.cy.js
@@ -1,6 +1,13 @@
 describe('Note app', function () {
 
   beforeEach(function () {
+    cy.request('POST', 'http://localhost:3001/api/testing/reset')
+    const user = {
+      name: 'Matti Luukkainen',
+      username: 'mluukkai',
+      password: 'salainen'
+    }
+    cy.request('POST', 'http://localhost:3001/api/users/', user)
     cy.visit('http://localhost:5173')
   })
 
@@ -15,7 +22,7 @@ describe('Note app', function () {
 
   it('user can log in', function () {
     cy.contains('log in').click()
-    cy.get('#username').type('root')
+    cy.get('#username').type('mluukkai')
     cy.get('#password').type('salainen')
     cy.get('#login-button').click()
   })
@@ -23,7 +30,7 @@ describe('Note app', function () {
   describe('when logged in', function () {
     beforeEach(function () {
       cy.get('button').contains('log in').click()
-      cy.get('input:first').type('root')
+      cy.get('input:first').type('mluukkai')
       cy.get('input:last').type('salainen')
       cy.get('#login-button').click()
     })
@@ -35,6 +42,22 @@ describe('Note app', function () {
       cy.contains('save').click()
       cy.contains('a note created by cypress')
     })
-  })
 
+    describe('and a note exists', function () {
+      beforeEach(function () {
+        cy.get('button').contains('new note').click()
+        cy.get('input').type('another note cypress')
+        cy.contains('save').click()
+      })
+
+      it('it can be made not important', function () {
+        cy.contains('another note cypress')
+          .contains('make not important')
+          .click()
+
+        cy.contains('another note cypress')
+          .contains('make important')
+      })
+    })
+  })
 })
