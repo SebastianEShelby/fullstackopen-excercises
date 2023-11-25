c384fba9c02a7092d8d3288dc85ec4291acefb50
Sebastian Shelby <sebastianeshelby@gmail.com>
2023-11-24 20:23:26 -0500

    part 5.17: bloglist end to end testing, step1

--- part5/bloglist-frontend/cypress/e2e/bloglist.cy.js
+++ part5/bloglist-frontend/cypress/e2e/bloglist.cy.js
@@ -1,5 +1,12 @@
-describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
+describe('Blog app', function () {
+  beforeEach(function () {
+    cy.request('POST', 'http://localhost:3001/api/testing/reset')
+    cy.visit('http://localhost:5173')
+  })
+
+  it('Login form is shown', function () {
+    cy.get('#username').should('be.visible')
+    cy.get('#password').should('be.visible')
+    cy.get('#login-button').should('be.visible')
   })
 })
--- part5/bloglist-frontend/src/components/Login.jsx
+++ part5/bloglist-frontend/src/components/Login.jsx
@@ -6,6 +6,7 @@ export const Login = ({ username, password, setUsername, setPassword, handleLogi
         <div>
           username
           <input
+            id="username"
             type="text"
             value={username}
             name="Username"
@@ -15,13 +16,14 @@ export const Login = ({ username, password, setUsername, setPassword, handleLogi
         <div>
           password
           <input
+            id="password"
             type="password"
             value={password}
             name="Password"
             onChange={({ target }) => setPassword(target.value)}
           />
         </div>
-        <button type="submit">login</button>
+        <button id="login-button" type="submit">login</button>
       </form>
     </>
   )
