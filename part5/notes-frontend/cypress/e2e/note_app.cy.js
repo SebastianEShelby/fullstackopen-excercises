48bae10a49abf0f10a5ba3b4e998ec3390a6d4c2
Sebastian Shelby <sebastianeshelby@gmail.com>
2023-11-24 19:37:52 -0500

    part 5d Testing new note form

--- part5/notes-frontend/cypress/e2e/note_app.cy.js
+++ part5/notes-frontend/cypress/e2e/note_app.cy.js
@@ -19,4 +19,22 @@ describe('Note app', function () {
     cy.get('#password').type('salainen')
     cy.get('#login-button').click()
   })
+
+  describe('when logged in', function () {
+    beforeEach(function () {
+      cy.get('button').contains('log in').click()
+      cy.get('input:first').type('root')
+      cy.get('input:last').type('salainen')
+      cy.get('#login-button').click()
+    })
+
+
+    it('a new note can be created', function () {
+      cy.get('button').contains('new note').click()
+      cy.get('input').should('be.visible').type('a note created by cypress')
+      cy.contains('save').click()
+      cy.contains('a note created by cypress')
+    })
+  })
+
 })
