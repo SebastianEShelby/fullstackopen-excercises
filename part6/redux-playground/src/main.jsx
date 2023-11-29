67e415a0ac9dc1e6e2ceb56ac34675fc4ba4386c
Sebastian Shelby <sebastianeshelby@gmail.com>
2023-11-29 10:27:28 -0500

    part 6a Uncontrolled form

--- part6/redux-playground/src/main.jsx
+++ part6/redux-playground/src/main.jsx
@@ -1,6 +1,6 @@
 import ReactDOM from 'react-dom/client'
 import { createStore } from 'redux'
-import { noteReducer } from './reducers/noteReducer'
+import noteReducer from './reducers/noteReducer'
 
 const store = createStore(noteReducer)
 
@@ -22,12 +22,46 @@ store.dispatch({
   }
 })
 
+const generateId = () =>
+  Number((Math.random() * 1000000).toFixed(0))
+
 const App = () => {
+
+  const addNote = (event) => {
+    event.preventDefault()
+    const content = event.target.note.value
+    event.target.note.value = ''
+    store.dispatch({
+      type: 'NEW_NOTE',
+      payload: {
+        content,
+        important: false,
+        id: generateId()
+      }
+    })
+  }
+
+
+  const toggleImportance = (id) => {
+    store.dispatch({
+      type: 'TOGGLE_IMPORTANCE',
+      payload: { id }
+    })
+  }
+
   return (
     <div>
+
+      <form onSubmit={addNote}>
+        <input name="note" />
+        <button type="submit">add</button>
+      </form>
       <ul>
         {store.getState().map(note =>
-          <li key={note.id}>
+          <li
+            key={note.id}
+            onClick={() => toggleImportance(note.id)}
+          >
             {note.content} <strong>{note.important ? 'important' : ''}</strong>
           </li>
         )}
