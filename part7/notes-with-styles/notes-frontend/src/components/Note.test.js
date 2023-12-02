08044c50757f014be5171a0240f4b6e42f0f3fe5
Sebastian Shelby <sebastianeshelby@gmail.com>
2023-12-02 15:33:12 -0500

    add bootstrap version of notes app

---
+++ part7/notes-with-styles/notes-frontend/src/components/Note.test.js
@@ -0,0 +1,40 @@
+import React from 'react'
+import '@testing-library/jest-dom'
+import { render, screen } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
+import Note from './Note'
+
+test('renders content', () => {
+  const note = {
+    content: 'Component testing is done with react-testing-library',
+    important: true
+  }
+
+  render(<Note note={note} />)
+
+  const element = screen.getByText('Component testing is done with react-testing-library')
+
+  screen.debug(element)
+
+  expect(element).toBeDefined()
+})
+
+test('clicking the button calls event handler once', async () => {
+  const note = {
+    content: 'Component testing is done with react-testing-library',
+    important: true
+  }
+
+  // The event handler is a "mock" function defined with Jest
+  const mockHandler = jest.fn()
+
+  render(
+    <Note note={note} toggleImportance={mockHandler} />
+  )
+  // A session is started to interact with the rendered component
+  const user = userEvent.setup()
+  const button = screen.getByText('make not important')
+  await user.click(button)
+
+  expect(mockHandler.mock.calls).toHaveLength(1)
+})
