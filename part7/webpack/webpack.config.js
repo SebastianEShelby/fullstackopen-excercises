997b28eca641eda6e4132535d5d924c6661e36dd
Sebastian Shelby <sebastianeshelby@gmail.com>
2023-12-02 17:17:27 -0500

    part 7d Development and production configuration

--- part7/webpack/webpack.config.js
+++ part7/webpack/webpack.config.js
@@ -1,6 +1,12 @@
 const path = require('path')
+const webpack = require('webpack')
 
-const config = () => {
+const config = (env, argv) => {
+
+  const backend_url = argv.mode === 'production'
+    ? 'https://notes2023.fly.dev/api/notes'
+    : 'http://localhost:3001/notes'
+
   return {
     entry: './src/index.js',
     output: {
@@ -27,7 +33,13 @@ const config = () => {
           use: ['style-loader', 'css-loader'],
         },
       ],
-    }
+    },
+
+    plugins: [
+      new webpack.DefinePlugin({
+        BACKEND_URL: JSON.stringify(backend_url)
+      })
+    ]
   }
 }
 
