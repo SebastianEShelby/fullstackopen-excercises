61c9c4b689c2644d2882256b58b53f98285ce919
Sebastian Shelby <sebastianeshelby@gmail.com>
2023-12-02 16:50:57 -0500

    part 7d CSS

--- part7/webpack/src/App.js
+++ part7/webpack/src/App.js
@@ -1,11 +1,7 @@
-import React from 'react' // we need this now also in component files
-
 const App = () => {
   return (
-    <div>
+    <div className="container">
       hello webpack
     </div>
   )
 }
-
-export default App
