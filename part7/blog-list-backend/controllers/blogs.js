d7bc5adf50e6518b69c7e26544412456f311a668
Sebastian Shelby <sebastianeshelby@gmail.com>
2023-12-03 20:47:54 -0500

    fix a bug where user blogs were not updated after blog was deleted

--- part7/blog-list-backend/controllers/blogs.js
+++ part7/blog-list-backend/controllers/blogs.js
@@ -2,7 +2,11 @@ const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
 
 blogsRouter.get('/', async (request, response) => {
-  const blogs = await Blog.find({}).populate('user', { username: 1, name: 1, id: 1 })
+  const blogs = await Blog.find({}).populate('user', {
+    username: 1,
+    name: 1,
+    id: 1,
+  })
   response.json(blogs)
 })
 
@@ -11,7 +15,7 @@ blogsRouter.post('/', async (request, response) => {
   const blog = new Blog({
     ...request.body,
     likes: request.body.likes || 0,
-    user: user._id
+    user: user._id,
   })
 
   const savedBlog = await blog.save()
@@ -28,9 +32,19 @@ blogsRouter.delete('/:id', async (request, response) => {
 
   let blogToDelete = await Blog.findById(request.params.id)
 
-  if (blogToDelete.user.toString() !== user.id) return response.status(401).json({ error: 'Unable to delete this blog since you are NOT the user who created it!' })
+  if (blogToDelete.user.toString() !== user.id)
+    return response.status(401).json({
+      error:
+        'Unable to delete this blog since you are NOT the user who created it!',
+    })
 
   await Blog.findByIdAndDelete(request.params.id)
+
+  user.blogs = user.blogs.filter(
+    (blogId) => blogId.toString() !== request.params.id,
+  )
+  await user.save()
+
   response.status(204).end()
 })
 
@@ -43,10 +57,12 @@ blogsRouter.put('/:id', async (request, response) => {
     author: body.author,
     url: body.url,
     likes: body.likes,
-    user: user._id
+    user: user._id,
   }
 
-  const updatedBlog = await Blog.findByIdAndUpdate(request.params.id, blog, { new: true })
+  const updatedBlog = await Blog.findByIdAndUpdate(request.params.id, blog, {
+    new: true,
+  })
   updatedBlog.populate('user', { username: 1, name: 1, id: 1 })
 
   user.blogs = user.blogs.concat(updatedBlog._id)
